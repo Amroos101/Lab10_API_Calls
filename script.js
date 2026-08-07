28311391e36a3a7f2978474772af25ff8585205d
Amroos101 <omayowps@gmail.com>
2026-08-06 21:36:54 -0400

    Add XMLHttpRequest to fetch and display post data

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ document.getElementById("fetchBtn").addEventListener("click", function() {
                 "<span class='error-msg'>something went wrong: " + error.message + "</span>";
         });
 });
+document.getElementById("xhrBtn").addEventListener("click", function() {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/posts/2", true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                const data = JSON.parse(xhr.responseText);
+                document.getElementById("output").innerHTML =
+                    "<strong>Title:</strong> " + data.title + "<br><strong>Body:</strong> " + data.body;
+            } else {
+                document.getElementById("output").innerHTML =
+                    "<span class='error-msg'>error loading data, status: " + xhr.status + "</span>";
+            }
+        }
+    };
+    xhr.send();
+});
